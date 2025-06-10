51094854b171621306a1528401661afbdcce443a
JohnTan12345 <s10259175@connect.np.edu.sg>
2025-06-10 22:14:02 +0800

    Implemented the Coin

--- Scripts/CoinBehaviour.cs
+++ Scripts/CoinBehaviour.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 
 public class CoinBehaviour : MonoBehaviour
 {
+    // Variables
+    public void AddCoin(PlayerBehaviour player)
+    {
+        player.coins++;
+        Destroy(gameObject);
+    }
     private void Start()
     {
         StartCoroutine(RotateCoin());
     }
-    IEnumerator RotateCoin()
-    { 
+    IEnumerator RotateCoin() // Spins the coin every frame
+    {
         while (true)
         {
             transform.Rotate(0, 0, 0.1f);
-            yield return null; // Wait for the next frame
+            yield return null;
         }
     }
 
--- Scripts/PlayerBehaviour.cs
+++ Scripts/PlayerBehaviour.cs
@@ -1,16 +1,51 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Variables
+    [SerializeField]
+    private int health = 100;
+    public int coins = 0;
+    [SerializeField]
+    private List<GameObject> items = new();
+    private bool interactable = false;
+    private GameObject interactableObject;
+    [SerializeField]
+    private GameObject raycastSpawner;
+
+    void FixedUpdate()
     {
-        
+        RaycastHit hit;
+
+        if (Physics.Raycast(raycastSpawner.transform.position, raycastSpawner.transform.forward, out hit, 1.5f) && (hit.collider.CompareTag("Collectible"))) // May Add Interactible objects later
+        {
+            interactable = true;
+            interactableObject = hit.collider.gameObject;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnInteract()
     {
-        
+        if (interactable)
+        {
+            if (interactableObject.CompareTag("Collectible"))
+            {
+                if (interactableObject.GetComponent<CoinBehaviour>() != null)
+                {
+                    interactableObject.GetComponent<CoinBehaviour>().AddCoin(this);
+                    Interacted();
+                }
+            }
+        }
     }
+
+    void Interacted()
+    {
+        interactable = false;
+        interactableObject = null;
+    }
+
 }
